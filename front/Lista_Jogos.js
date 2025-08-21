273fdac969f96ac3aeebfcb2801f7b317823b843
DerekMA15 <124485276+DerekMA15@users.noreply.github.com>
2025-08-20 22:29:22 -0300

    primeiro commit

---
+++ front/Lista_Jogos.js
@@ -0,0 +1,34 @@
+// Lista de Jogos base pare testes nas funçãos
+const minhaLista = [
+  {
+    nome: "The Witcher 3: Wild Hunt",
+    capa: "https://url-da-capa-do-jogo.com/witcher3.jpg",
+    genero: ["RPG", "Ação", "Mundo Aberto"],
+    developer: "CD Projekt Red",
+    dataDeLancamento: "2015-05-19",
+    plataforma: ["PC", "PlayStation 4", "Xbox One", "Nintendo Switch"],
+    estaNaMyList: true,
+    estaNosDesejos: false,
+  },
+  {
+    nome: "Hades",
+    capa: "https://url-da-capa-do-jogo.com/hades.jpg",
+    genero: ["Roguelike", "Ação", "Indie"],
+    developer: "Supergiant Games",
+    dataDeLancamento: "2020-09-17",
+    plataforma: ["PC", "Nintendo Switch", "PlayStation", "Xbox"],
+    estaNaMyList: false,
+    estaNosDesejos: true,
+  },
+
+  {
+    nome: "Stardew Valley",
+    capa: "https://url-da-capa-do-jogo.com/stardew.jpg",
+    genero: ["Simulação", "RPG", "Indie"],
+    developer: "ConcernedApe",
+    dataDeLancamento: "2016-02-26",
+    plataforma: ["PC", "Mobile", "PlayStation 4", "Xbox One", "Nintendo Switch"],
+    estaNaMyList: true,
+    estaNosDesejos: false,
+  }
+]
